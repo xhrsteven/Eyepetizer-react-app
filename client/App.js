9d571075b92413a23509281fa8f33ac2bc31181e
Steven <xhrsteven@gmail.com>
2018-07-26 18:31:01 -0500

    Update client

---
+++ client/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Landing from './corePage/Landing';
+
+class App extends Component {
+    render(){
+        return(
+            <Landing />
+        )
+    }
+}
